c4e7191803e58f44134217c2b4c9378c1c402456
shubaivan <shuba.ivan@yandex.ru>
2015-04-17 10:21:33 +0300

    fix test

--- src/AppBundle/Tests/Controller/TagControllerTest.php
+++ src/AppBundle/Tests/Controller/TagControllerTest.php
@@ -4,15 +4,15 @@ namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class PostControllerTest extends WebTestCase
+class TagControllerTest extends WebTestCase
 {
     public function testIndexAction()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/tag/symfony');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertTrue($crawler->filter('html:contains("Wall")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Список проектов тегу symfony")')->count() > 0);
     }
 }
