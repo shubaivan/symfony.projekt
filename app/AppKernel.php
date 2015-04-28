6780e44f49498eb0f09db5f75ace6c9a126df8ff
shubaivan <shuba.ivan@yandex.ru>
2015-04-28 12:05:53 +0300

    add UserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             new Iphp\FileStoreBundle\IphpFileStoreBundle(),
             new Hautelook\AliceBundle\HautelookAliceBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            new UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
