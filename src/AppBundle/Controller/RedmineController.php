56fc6e973ad074c9d1672eef0de3018bb9a31746
shubaivan <shuba.ivan@yandex.ru>
2015-05-08 22:37:39 +0300

    create service and work projectAction

--- src/AppBundle/Controller/RedmineController.php
+++ src/AppBundle/Controller/RedmineController.php
@@ -14,6 +14,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template as Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Service\Redmine;
 use AppBundle\Service\Client;
+use AppBundle\Service\Project;
 
 class RedmineController extends Controller
 {
@@ -24,18 +25,11 @@ class RedmineController extends Controller
      */
     public function projectAction()
     {
-        $url = 'https://redmine.ekreative.com';
-        $apiKey = '2fda745bb4cdd835fdf41ec1fab82a13ddc1a54c';
-        $httpAuthString = 'test';
-
-        $client = new \Redmine\Client($url, $apiKey, $httpAuthString);
-
-        $project = $client->api('project')->all();
-
-        var_dump($project);
+        $name = $this->get('my_project');
+        $projectListName = $name->Name();
 
         return array(
-            "projects" => $project,
+            "projectListName" => $projectListName,
         );
     }
 
@@ -62,5 +56,62 @@ class RedmineController extends Controller
         return array(
             "fullIssues" => $fullIssue,
         );
+    }
+
+    /**
+     * @Route("/new/issues", name="new_issues")
+     * @Method(methods={"GET", "POST"})
+     * @Template()
+     */
+    public function newIssuesAction()
+    {
+        $url = 'https://redmine.ekreative.com';
+        $apiKey = '2fda745bb4cdd835fdf41ec1fab82a13ddc1a54c';
+        $httpAuthString = 'test';
+
+        $client = new \Redmine\Client($url, $apiKey, $httpAuthString);
+
+        $ret = $client->api('issue')->create(array(
+            'project_id'     => 'test',
+            'subject'        => 'test api (xml) 3',
+            'description'    => 'test api',
+            'assigned_to_id' => 'user1',
+//            'custom_fields'  => array(
+//                array(
+//                    'id'    => 2,
+//                    'name'  => 'Issuer',
+//                    'value' => $_POST['ISSUER'],
+//                ),
+//                array(
+//                    'id'    => 5,
+//                    'name'  => 'Phone',
+//                    'value' => $_POST['PHONE'],
+//                ),
+//                array(
+//                    'id'    => '8',
+//                    'name'  => 'Email',
+//                    'value' => $_POST['EMAIL'],
+//                ),
+//            ),
+//            'watcher_user_ids' => array(),
+        ));
+
+        if( $ret instanceof SimpleXMLElement ){
+            //look in the Object
+            var_dump($ret);
+        }
+        else{
+            if( $ret === true ){
+                echo "success";
+            }
+            else{
+                echo "error";
+            }
+        }
+//        var_dump($ret);
+//        echo $ret->asXML();
+//        return array(
+//            "fullIssues" => $fullIssue,
+//        );
     }
 }
